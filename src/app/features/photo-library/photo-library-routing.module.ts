86861352a5bb2647b08d01808f881bc357aad3cd
Milos Jokovic <milosjoka@gmail.com>
2023-03-16 15:23:28 +0100

    Add component favorites

--- src/app/features/photo-library/photo-library-routing.module.ts
+++ src/app/features/photo-library/photo-library-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {PhotosComponent} from "./pages/photos/photos.component";
+import {FavoritesComponent} from "./pages/favorites/favorites.component";
 
 const routes: Routes = [
   {
     path: '',
     component: PhotosComponent
+  },
+  {
+    path: 'favorites',
+    component: FavoritesComponent
   }
 ];
 
--- src/app/features/photo-library/photo-library.module.ts
+++ src/app/features/photo-library/photo-library.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { PhotoLibraryRoutingModule } from './photo-library-routing.module';
 import { PhotosComponent } from './pages/photos/photos.component';
+import { FavoritesComponent } from './pages/favorites/favorites.component';
 
 
 @NgModule({
   declarations: [
-    PhotosComponent
+    PhotosComponent,
+    FavoritesComponent
   ],
   imports: [
     CommonModule,
